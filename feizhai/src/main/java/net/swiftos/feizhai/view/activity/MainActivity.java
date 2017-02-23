79ce6dd00993ff75d9fdffca68d02e905d420f11
Swift Gan (RD-CN-INTRN) <swift_gan@trendmicro.com.cn>
2017-02-23 17:08:38 +0800

    1.add AspectJ

--- feizhai/src/main/java/net/swiftos/feizhai/view/activity/MainActivity.java
+++ feizhai/src/main/java/net/swiftos/feizhai/view/activity/MainActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import net.swiftos.feizhai.R;
+import net.swiftos.usermodule.aop.DebugLog;
 
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    @DebugLog
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
